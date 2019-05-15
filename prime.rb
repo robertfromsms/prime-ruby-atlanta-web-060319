6e6e0cbcfbff1cecb80c77882a059c5cccad402a
Jau-Tzuoo Chen <robertfromsms@gmail.com>
2019-05-15 04:03:44 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,6 @@
 def prime?(int)
   prime_range_array = Array(2..Math.sqrt(int))
-  prime_range_array.each |element| do
+  prime_range_array.each do |element|
     if int % element == 0
       return false
   end
