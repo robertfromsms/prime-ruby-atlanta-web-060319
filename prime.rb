ab4a253490d6144643d4db3bd0360cc655256076
Jau-Tzuoo Chen <robertfromsms@gmail.com>
2019-05-15 04:09:17 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+def prime?(int)
+  if int <= 1
+    return false
+  end
+  prime_range_array = Array(2..Math.sqrt(int))
+  prime_range_array.each do |element|
+    if int % element == 0
+      return false
+    end
+  end
+  return true
+end
